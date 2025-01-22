2d177f970155611c83747f2f473a8f28aafc8ba8
Yahya Badran <yahya.badran@gmail.com>
2025-01-22 07:57:45 -0600

    add setup.py and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+Work in progress on live updating themes for alacritty by scrolling and searching between theme names
+
+## Setup
+clone and then
+```console 
+pipx install -e .
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='alacritty_theme_scroll',
+    version='0.0.2',
+    author='Yahya Badran',
+    author_email='techtweaking@gmail.com',
+    description='pick your alacritty theme by scrolling and live updating',
+    url='https://github.com/badranx/alacritty-theme-scroll',
+    install_requires=['GitPython >=3.1.42'],
+    packages = [package for package in find_packages() if package.startswith("alacritty_theme_scroll")],
+    entry_points={
+    'console_scripts': [
+        'alacritty_theme_scroll = alacritty_theme_scroll.main:run',
+    ],},
+    license='MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
